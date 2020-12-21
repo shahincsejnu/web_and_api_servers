f92b06c54e0d22d030135b90c6b142d50fd3bca8
sahadat_hossain <sahadat_hossain@appscode.com>
2020-12-21 16:16:59 +0600

    C3: REST API version1.0.1 - net/http router

--- RESTful_API_with_go/api.go
+++ RESTful_API_with_go/api.go
@@ -1,6 +1,9 @@
+// from : https://tutorialedge.net/golang/creating-restful-api-with-golang/
+
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,8 +13,34 @@ func homepage(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Homepage!")
 }
 
+func returnAllArticles(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(Articles)
+}
+
+type Article struct {
+	Title string `json:"title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
 func main() {
+	Articles = []Article{
+		{
+			Title : "Hello",
+			Desc : "Article Description",
+			Content : "Article Contents",
+		},
+		{
+			Title : "Hello 2",
+			Desc : "Article Description",
+			Content : "Article Content",
+		},
+	}
+
 	http.HandleFunc("/", homepage)
+	http.HandleFunc("/articles", returnAllArticles)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
